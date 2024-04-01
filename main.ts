ccc204bbb90da4829bf8c3514132052d25049521
roshnimahesh <roshnimahesh098@gmail.com>
2024-04-01 05:10:13 -0700

    done assgnment

---
+++ main.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+
+let todo = []
+
+let condition = true;
+
+while (condition)
+
+{
+    let addTask = await inquirer.prompt(
+    [
+        {
+            name:"todo",
+            type:"input",
+            message:"what you want to add in your todo?"
+        },
+
+        {
+
+            name:"addmore",
+            type:"confrim",
+            message:"do you want to add more?",
+            default: "true"
+        }
+    ]
+);
+
+todo.push(addTask.todo); 
+condition = addTask.addmore
+console.log(todo)
+};
